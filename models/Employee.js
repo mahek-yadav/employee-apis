d7c5fb65eab903a98d929ff07ce03b4d5827caee
Mahek yadav <maheky2306@icloud.com>
2026-08-14 11:41:11 +0530

    Fix Employee schema

--- models/Employee.js
+++ models/Employee.js
@@ -1,26 +1,29 @@
-const mongoose=require('mongoose');
-const employeeSchema={
-    name:{
-        type:String,
-        required:true
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    department:{
-        type:String,
-        required:true
+    department: {
+        type: String,
+        required: true
     },
-    role:{
-        type:String,
-        required:true
+    role: {
+        type: String,
+        required: true
     },
-    salary:{
-        typr:String,
-        required:true
+    salary: {
+        type: String,
+        required: true
     }
-}
-const Employee=mongoose.model('Employee', employeeSchema);
-modeule.exports=Employee;
+});
+
+const Employee = mongoose.model('Employee', employeeSchema);
+
+module.exports = Employee;
